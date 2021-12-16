ac9c2ad6df7e064be3343c8d0b4d94e616097d7b
KanojiaDeepak <64652395+KanojiaDeepak@users.noreply.github.com>
2021-12-16 21:01:39 +0530

    Update settings.py

--- djangoChat/settings.py
+++ djangoChat/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n7nj4g^8#$a@hhm=rby=)ambqxqok$$!ou8t6z7!zxf2#e)ju4'
+SECRET_KEY = 'secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
